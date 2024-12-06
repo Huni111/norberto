21dd5de1998ab502437cb87112adc301544a8d92
Huni111 <huni19966@gmail.com>
2024-12-06 15:36:44 +0200

    image issue

--- norberto_tailor/src/components/Home.jsx
+++ norberto_tailor/src/components/Home.jsx
@@ -4,6 +4,7 @@ const Home = () => {
     return(
         <>
         <h1>Home</h1>
+        <img src="https://drive.google.com/uc?export=view&id=1edW5fCnXRKdCjidjxZ0NFuF0zB8cDQlK" alt="Your Image" />
         </>
     )
 }
